03f6bdb67c0ecd5e470ae47cb8864ba779f09a8b
Tim Skaggs <me@timskaggs.com>
2017-12-09 22:30:14 -0700

    Add menu and columns from Element UI

--- src/components/App/Header.js
+++ src/components/App/Header.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Menu, Button } from 'element-react';
+
+import 'element-theme-default';
+
+import logo from './logo.svg';
+import './style.css';
 
 const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/about'>About</Link></li>
-      </ul>
-    </nav>
-  </header>
+	<Menu theme="dark" defaultActive="1" className="el-menu-demo" mode="horizontal">
+	  <Link to='/'><Menu.Item index="1"><img src={logo} className="App-logo" alt="logo" /></Menu.Item></Link>
+	  <div className="App-right-menu">
+	  	<Link to='/about'><Menu.Item index="2">About</Menu.Item></Link>
+	  	<Menu.Item index="3"><Button type="primary" size="small">Wow!</Button></ Menu.Item>
+	  </div>
+	</Menu>
 )
 
 export default Header
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import { Layout } from 'element-react';
 
 import Header from './Header'
 import Main from './Main'
+
+import 'element-theme-default';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Main />
+      	<Layout.Row className="tac">
+      		<Layout.Col span={24}>
+        		<Header />
+        	</Layout.Col>
+        	<Layout.Col span={24}>
+        		<Main />
+        	</Layout.Col>
+       	</Layout.Row>
       </div>
     );
   }
